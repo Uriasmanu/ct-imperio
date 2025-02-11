3d7d09631eb9391599ab3274a4c36a3c2295c0a7
Manoela Urias <maanoelaurias@gmail.com>
2025-02-11 15:56:50 -0300

    ajuste do layaut e adição do s contatos

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,22 +1,35 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
-import { Home, LogOut, Package, PanelBottom, Settings, ShoppingBag, Users } from "lucide-react";
+import { Home, LibraryBig, MenuIcon, Package, Settings, ShoppingBag, Users } from "lucide-react";
 import { TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
 import { Tooltip } from "@radix-ui/react-tooltip";
+import Image from "next/image";
+import { RiFacebookCircleLine } from "react-icons/ri";
+import { BsInstagram } from "react-icons/bs";
+import { AiFillTikTok } from "react-icons/ai";
+import { LuMapPin } from "react-icons/lu";
+import { FaDumbbell, FaWhatsapp } from "react-icons/fa";
 
 export function Sidebar() {
     return (
         <div className="flex w-full flex-col bg-muted/40">
 
-            <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 border-r bg-background sm:flex flex-col">
+            <aside className="fixed inset-y-0 left-0 z-10 hidden w-20 border-r bg-background sm:flex flex-col bg-yellow-300">
                 <nav className="flex flex-col items-center gap-4 px-2 py-5">
                     <TooltipProvider>
                         <Link href="#"
-                            className="flex h-9 w-9 shrink-0 items-center justify-center bg-primary text-primary-foreground rounded-full"
+                            className="flex h-9 w-9 shrink-0 items-center justify-center text-primary-foreground rounded-full"
                         >
-                            <Package className="h-4 w-4" />
-                            <span className="sr-only">Dashbord Avatar</span>
+                            <Image
+                                src="/image/logo.png"
+                                alt="logo do ct imperio"
+                                className="w-10 h-10"
+                                width={180} // largura desejada em pixels
+                                height={180} // altura desejada em pixels
+                            />
+
+                            <span className="sr-only">logo do ct imperio</span>
                         </Link>
 
                         <Tooltip>
@@ -25,11 +38,11 @@ export function Sidebar() {
                                     className="flex h-9 w-9 shrink-0 items-center justify-center text-muted-foreground rounded-lg transition-colors hover:text-foreground"
                                 >
 
-                                    <Home className="h-5 w-5" />
-                                    <span className="sr-only">Inicio</span>
+                                    <LibraryBig className="h-5 w-5" />
+                                    <span className="sr-only">Sobre</span>
                                 </Link>
                             </TooltipTrigger>
-                            <TooltipContent side="right">Inicio</TooltipContent>
+                            <TooltipContent side="right">Sobre</TooltipContent>
                         </Tooltip>
 
                         <Tooltip>
@@ -38,24 +51,24 @@ export function Sidebar() {
                                     className="flex h-9 w-9 shrink-0 items-center justify-center text-muted-foreground rounded-lg transition-colors hover:text-foreground"
                                 >
 
-                                    <ShoppingBag className="h-5 w-5" />
-                                    <span className="sr-only">Pedidos</span>
+                                    <FaDumbbell className="h-5 w-5" />
+                                    <span className="sr-only">Aulas</span>
                                 </Link>
                             </TooltipTrigger>
-                            <TooltipContent side="right">Pedidos</TooltipContent>
+                            <TooltipContent side="right">Aulas</TooltipContent>
                         </Tooltip>
 
                         <Tooltip>
                             <TooltipTrigger asChild>
-                                <Link href="#"
+                                <Link href="https://wa.me/5514997856670?text=Ol%C3%A1%2C%20gostaria%20de%20saber%20mais%20de%20como%20funciona%20o%20CT%20Imp%C3%A9rio"
                                     className="flex h-9 w-9 shrink-0 items-center justify-center text-muted-foreground rounded-lg transition-colors hover:text-foreground"
                                 >
 
-                                    <Package className="h-5 w-5" />
-                                    <span className="sr-only">Produtos</span>
+                                    <FaWhatsapp className="h-5 w-5" />
+                                    <span className="sr-only">Contatos</span>
                                 </Link>
                             </TooltipTrigger>
-                            <TooltipContent side="right">Produtos</TooltipContent>
+                            <TooltipContent side="right">Contatos</TooltipContent>
                         </Tooltip>
 
                         <Tooltip>
@@ -65,65 +78,76 @@ export function Sidebar() {
                                 >
 
                                     <Users className="h-5 w-5" />
-                                    <span className="sr-only">Clientes</span>
+                                    <span className="sr-only">Professores</span>
                                 </Link>
                             </TooltipTrigger>
-                            <TooltipContent side="right">Clientes</TooltipContent>
+                            <TooltipContent side="right">Professores</TooltipContent>
                         </Tooltip>
 
-                        <Tooltip>
-                            <TooltipTrigger asChild>
-                                <Link href="#"
-                                    className="flex h-9 w-9 shrink-0 items-center justify-center text-muted-foreground rounded-lg transition-colors hover:text-foreground"
-                                >
+                        <div className="flex flex-col gap-4 py-4 mt-16">
+                                    <Link
+                                        href="https://www.facebook.com/share/165fabMdE9/?mibextid=qi2Omg"
+                                        className="flex items-center gap-4 px-2.5 hover:text-foreground"
+                                        prefetch={false}
+                                    >
+                                        <RiFacebookCircleLine className="h-5 w-5 transition-all" />
+                                    </Link>
 
-                                    <Settings className="h-5 w-5" />
-                                    <span className="sr-only">Configuração</span>
-                                </Link>
-                            </TooltipTrigger>
-                            <TooltipContent side="right">Configuração</TooltipContent>
-                        </Tooltip>
+                                    <Link
+                                        href="https://www.instagram.com/ct.imperio.fitness/"
+                                        className="flex items-center gap-4 px-2.5 hover:text-foreground"
+                                        prefetch={false}
+                                    >
+                                        <BsInstagram className="h-5 w-5 transition-all" />
+                                    </Link>
+
+                                    <Link
+                                        href="https://www.tiktok.com/@eusouwilll"
+                                        className="flex items-center gap-4 px-2.5 hover:text-foreground"
+                                        prefetch={false}
+                                    >
+                                        <AiFillTikTok className="h-5 w-5 transition-all" />
+                                    </Link>
+                                    <Link
+                                        href="https://www.google.com/maps/place/R.+Araraquara,+193+-+Alto+Cafezal,+Mar%C3%ADlia+-+SP,+17504-086/@-22.2131632,-49.9525712,17z/data=!3m1!4b1!4m6!3m5!1s0x94bfd75eea16093b:0x7312fa3826dd8775!8m2!3d-22.2131632!4d-49.9525712!16s%2Fg%2F11vjz5r1v1?entry=ttu&g_ep=EgoyMDI1MDIwOS4wIKXMDSoJLDEwMjExMjM0SAFQAw%3D%3D"
+                                        className="flex items-center gap-4 px-2.5 hover:text-foreground"
+                                        prefetch={false}
+                                    >
+                                        <LuMapPin className="h-5 w-5 transition-all" />
+                                    </Link>
+
+                                </div>
+
                     </TooltipProvider>
                 </nav>
 
-                <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-5">
-                    <TooltipProvider>
-                        <Tooltip>
-                            <TooltipTrigger asChild>
-                                <Link href="#"
-                                    className="flex h-9 w-9 shrink-0 items-center justify-center text-muted-foreground rounded-lg transition-colors hover:text-foreground"
-                                >
-
-                                    <LogOut className="h-5 w-5" />
-                                    <span className="sr-only">Sair</span>
-                                </Link>
-                            </TooltipTrigger>
-                            <TooltipContent side="right">Sair</TooltipContent>
-                        </Tooltip>
-                    </TooltipProvider>
-                </nav>
             </aside>
 
             <div className="sm:hidden flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-                <header className="sticky top-0 z-30 flex h-14 items-center px-4 border-b bg-background gap-4 sm:static sm:h-auto sm:border-0 sm-bg-transparent sm:px-6">
+                <header className="sticky top-0 z-30 flex h-14 items-center px-4 bg-background sm:static sm:h-auto sm:border-0 sm-bg-transparent sm:px-6">
                     <Sheet>
                         <SheetTrigger asChild>
-                            <Button size="icon" variant="outline" className="sm:hidden">
-                                <PanelBottom className="w-5 h-5" />
+                            <Button size="icon" variant="outline" className="sm:hidden border-0">
+                                <MenuIcon className="w-6 h-6" />
                                 <span className="sr-only">abrir / fechar menu</span>
                             </Button>
                         </SheetTrigger>
 
-                        <SheetContent side="left" className="sm:max-w-x flex flex-col justify-between">
+                        <SheetContent side="left" className="sm:max-w-x flex flex-col justify-between bg-yellow-300">
 
                             <nav className="grid gap-6 text-lg font-medium">
-                                <Link
-                                    href="#"
-                                    className="flex h-10 w-10 bg-primary rounded-full text-lg items-center justify-center text-primary-foreground md:text-base gap-2"
+                                <Link href="#"
+                                    className="flex h-9 w-9 shrink-0 items-center justify-center text-primary-foreground rounded-full"
                                     prefetch={false}
                                 >
-                                    <Package className="h-5 w-5 transition-all" />
-                                    <span className="sr-only">logo do projeto</span>
+                                    <Image
+                                        src="/image/logo.png"
+                                        alt="logo do ct imperio"
+                                        className="w-10 h-10"
+                                        width={180} // largura desejada em pixels
+                                        height={180} // altura desejada em pixels
+                                    />
+                                    <span className="sr-only">logo do ct imperio</span>
                                 </Link>
 
                                 <Link
@@ -131,8 +155,8 @@ export function Sidebar() {
                                     className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                                     prefetch={false}
                                 >
-                                    <Home className="h-5 w-5 transition-all" />
-                                    Inicio
+                                    <LibraryBig className="h-5 w-5 transition-all" />
+                                    Sobre
                                 </Link>
 
                                 <Link
@@ -140,17 +164,17 @@ export function Sidebar() {
                                     className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                                     prefetch={false}
                                 >
-                                    <ShoppingBag className="h-5 w-5 transition-all" />
-                                    Pedidos
+                                    <FaDumbbell className="h-5 w-5 transition-all" />
+                                    Aulas
                                 </Link>
 
                                 <Link
-                                    href="#"
+                                    href="https://wa.me/5514997856670?text=Ol%C3%A1%2C%20gostaria%20de%20saber%20mais%20de%20como%20funciona%20o%20CT%20Imp%C3%A9rio"
                                     className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                                     prefetch={false}
                                 >
-                                    <Package className="h-5 w-5 transition-all" />
-                                    Produtos
+                                    <FaWhatsapp className="h-5 w-5 transition-all" />
+                                    Contatos
                                 </Link>
 
                                 <Link
@@ -159,32 +183,48 @@ export function Sidebar() {
                                     prefetch={false}
                                 >
                                     <Users className="h-5 w-5 transition-all" />
-                                    Clientes
+                                    Professores
                                 </Link>
 
-                                <Link
-                                    href="#"
-                                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                                    prefetch={false}
-                                >
-                                    <Settings className="h-5 w-5 transition-all" />
-                                    Configuração
-                                </Link>
+                                <div className="flex py-4">
+                                    <Link
+                                        href="https://www.facebook.com/share/165fabMdE9/?mibextid=qi2Omg"
+                                        className="flex items-center gap-4 px-2.5 hover:text-foreground"
+                                        prefetch={false}
+                                    >
+                                        <RiFacebookCircleLine className="h-5 w-5 transition-all" />
+                                    </Link>
+
+                                    <Link
+                                        href="https://www.instagram.com/ct.imperio.fitness/"
+                                        className="flex items-center gap-4 px-2.5 hover:text-foreground"
+                                        prefetch={false}
+                                    >
+                                        <BsInstagram className="h-5 w-5 transition-all" />
+                                    </Link>
+
+                                    <Link
+                                        href="https://www.tiktok.com/@eusouwilll"
+                                        className="flex items-center gap-4 px-2.5 hover:text-foreground"
+                                        prefetch={false}
+                                    >
+                                        <AiFillTikTok className="h-5 w-5 transition-all" />
+                                    </Link>
+                                    <Link
+                                        href="https://www.google.com/maps/place/R.+Araraquara,+193+-+Alto+Cafezal,+Mar%C3%ADlia+-+SP,+17504-086/@-22.2131632,-49.9525712,17z/data=!3m1!4b1!4m6!3m5!1s0x94bfd75eea16093b:0x7312fa3826dd8775!8m2!3d-22.2131632!4d-49.9525712!16s%2Fg%2F11vjz5r1v1?entry=ttu&g_ep=EgoyMDI1MDIwOS4wIKXMDSoJLDEwMjExMjM0SAFQAw%3D%3D"
+                                        className="flex items-center gap-4 px-2.5 hover:text-foreground"
+                                        prefetch={false}
+                                    >
+                                        <LuMapPin className="h-5 w-5 transition-all" />
+                                    </Link>
+
+                                </div>
                             </nav>
 
-                            <nav className="mt-auto flex flex-col gap-4 px-2 py-5">
-                                <Link
-                                    href="#"
-                                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                                    prefetch={false}
-                                >
-                                    <LogOut className="h-5 w-5 transition-all" />
-                                    Sair
-                                </Link></nav>
                         </SheetContent>
                     </Sheet>
 
-                    <h2>Menu</h2>
+                    <h1 className="font-semibold m-auto">CT Imperio</h1>
                 </header>
             </div>
         </div>
