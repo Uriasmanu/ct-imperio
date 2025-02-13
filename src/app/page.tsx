20ed72e5c5791dc608c57321a61aee6acb170c5b
Manoela Urias <maanoelaurias@gmail.com>
2025-02-13 00:21:03 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import { CalendarClock, Clock } from "lucide-react";
 import Image from "next/image";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-
 export default function Home() {
   return (
     <div className="bg-slate-800 flex flex-col sm:flex-row">
